c8f1c6ad3e8dd3542829644ee6106c0f5622e6a3
BluShine <umbreon27@gmail.com>
2015-01-24 21:03:03 -0500

    Initial Game
    Soda shaking game with wiimote.

---
+++ WhatWiiDo/MainGame.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WiimoteLib;
+using System.Media;
+
+namespace WhatWiiDo
+{
+    class MainGame
+    {
+        Dictionary<Guid, Wiimote> players;
+        WiimoteCollection wiimoteCollection;
+        Boolean running = true;
+        static float FPS = 100;
+
+        Minigame currentGame;
+
+        public MainGame()
+        {
+            Load();
+
+            List<Minigame> gameList = new List<Minigame>();
+            gameList.Add(new SodaGame());
+
+            currentGame = gameList[0];
+
+            float frameMilis = 1000f / FPS;
+
+            while (running)
+            {
+                DateTime last = DateTime.Now;
+
+                currentGame.update(players);
+                if (currentGame.isOver())
+                {
+                    gameList.Remove(currentGame);
+                    if (gameList.Count > 0)
+                    {
+                        currentGame = gameList[0];
+                    }
+                    else
+                    {
+                        running = false;
+                    }
+                }
+
+                TimeSpan elapsed = DateTime.Now - last;
+               System.Threading.Thread.Sleep((int) (frameMilis - elapsed.Milliseconds));
+            }
+
+            Console.ReadLine();
+        }
+
+        private void Load()
+        {
+            wiimoteCollection = new WiimoteCollection();
+            players = new Dictionary<Guid, Wiimote>();
+            int index = 1;
+
+            try
+            {
+                wiimoteCollection.FindAllWiimotes();
+            }
+            catch (WiimoteNotFoundException ex)
+            {
+                Console.WriteLine("WiiMote not found" + ex.Message);
+            }
+            catch(WiimoteException ex)
+			{
+                Console.WriteLine("Wiimote error" + ex.Message);
+            }
+			catch(Exception ex)
+			{
+                Console.WriteLine("Unknown error" + ex.Message);
+            }
+
+            foreach(Wiimote mote in wiimoteCollection) {
+
+                //set callback function
+                mote.WiimoteChanged += moteChanged;
+
+                //connect it
+                mote.Connect();
+                if(mote.WiimoteState.ExtensionType == ExtensionType.BalanceBoard) {
+                    Console.WriteLine("Why would you connect a balance board!?!?");
+                } 
+                else {
+                    mote.SetReportType(InputReport.IRExtensionAccel, IRSensitivity.Maximum, true);
+                }
+
+                //add it to our dictionary
+                players.Add(mote.ID, mote);
+
+                mote.SetLEDs(index++);
+            }
+        }
+
+        void moteChanged(object sender, WiimoteChangedEventArgs args)
+        {
+            Wiimote mote = (Wiimote) sender;
+            //Console.WriteLine(mote.ID + " did a thing.");
+        }
+    }
+}
---
+++ WhatWiiDo/SodaGame.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WiimoteLib;
+
+namespace WhatWiiDo
+{
+    public class SodaGame : Minigame
+    {
+        static int shakesNeeded = 50;
+        int shakes = 0;
+        bool lastShakeUp = true;
+
+        public SodaGame()
+        {
+        }
+
+        public void update(Dictionary<Guid, Wiimote> players)
+        {
+            foreach (Wiimote mote in players.Values)
+            {
+                if (mote.WiimoteState.AccelState.Values.Y < -2 && !lastShakeUp)
+                {
+                    Console.WriteLine();
+                    int i = shakes;
+                    while (i > 0)
+                    {
+                        Console.Write(" ");
+                        i--;
+                    }
+                    Console.Write("fap");
+                    lastShakeUp = true;
+                    shakes++;
+                }
+                if (mote.WiimoteState.AccelState.Values.Y > 2 && lastShakeUp)
+                {
+                    lastShakeUp = false;
+                }
+            }
+        }
+
+        public bool isOver()
+        {
+            if (shakes > shakesNeeded)
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("you win!");
+                return true;
+            }
+            return false;
+        }
+    }
+}
